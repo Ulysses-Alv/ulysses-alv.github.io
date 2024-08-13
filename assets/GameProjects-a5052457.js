3692f26eb57b5e8f9015373b24329c7a4fadc5f1
Ulises Alvarenga <ulises.j.alvarenga@gmail.com>
2024-08-13 12:40:25 -0300

    Updates

---
+++ assets/GameProjects-a5052457.js
@@ -0,0 +1,127 @@
+import{P as e,a as i}from"./ProjectData-51280c79.js";import{e as r,_ as o,h as n,c as l,i as c,a,d as g,o as p}from"./index-c0d6663b.js";const h=[new e("project-1","Grand Theft Laugh","img/projects/GrandTheftLaugh.gif",`
+    <div class="paragraph">
+     <strong>Grand Theft Laugh</strong> Global Game Jam 2024.
+    </div>
+    <div class="paragraph center">
+        <img src="img/projects/GrandTheftLaugh.gif">
+    </div>
+    <div class="paragraph center">
+        <a href="https://play.google.com/store/apps/details?id=some.playstore.thing" target="_blank">
+            <img src="img/projects/play-store-logo.png" alt="Play Store badge" />
+        </a>
+    </div>
+    <div class="paragraph">
+        <ul>
+        <li>Theme: Make me Laugh.</li>
+        <li>Language: C#.</li>
+        <li>Engine: Unity 2022.</li>
+        <li>Platform: Windows.</li>
+        <li>Other: GitHub, Git, Trello.</li></ul>
+    </div>
+
+    <div class="paragraph center">
+    <img class="phone-screenshot" src="https://ggjv4.s3.us-west-1.amazonaws.com/files/styles/flexslider_full/s3/games/2024/716762/screenshot/image.png?VersionId=85f.2y4vjdylbQqP.ej6LRvIkf3E_2De&itok=TFKakB3N"
+                                alt="img 1">
+        <img class="pc-screenshot" src="https://ggjv4.s3.us-west-1.amazonaws.com/files/styles/sidebar_full/s3/games/2024/716762/featured/plinplin.png?VersionId=Umed6w67zqJrAbT9yAKyDexECWuVsknb&itok=3r9G9-gO"
+                                alt="img 2">
+       
+    </div>
+    `,"#23bd69",!0,!0),new e("project-3","Farmoxel","img/projects/Carrot.png",`
+    <div class="paragraph">
+        <strong>Farmoxel</strong> is a thing of beauty that I am so proud of. I could write about it for hours.
+        <br/>Image by <a target="_blank" href="https://www.pexels.com/fr-fr/@miphotography">Miesha Maiden</a>.
+    </div>
+    <div class="paragraph center">
+    video
+        <iframe src="https://www.linkedin.com/embed/feed/update/urn:li:ugcPost:7097632850123866113?compact=1" 
+        class="youtube" frameborder="0" allowfullscreen></iframe>
+    </div>
+
+    <div class="paragraph">
+        Main features :
+        <ul>
+            <li>Language: C#.</li>
+            <li>Engine: Unity.</li>
+            <li>Platform: Windows.</li>
+            <li>Other: GitHub, Git, Trello.</li>
+        </ul>
+    </div>
+
+    <div class="paragraph">
+        <div class="notice">
+        Still in develop! Stay tuned on <a href="https://farteamdev.blogspot.com/" target="_blank">Blogspot</a>
+        </div>
+    </div>
+
+    <div class="paragraph center">
+        <img class="pc-screenshot" src="https://blogger.googleusercontent.com/img/a/AVvXsEhzNLWXAdOqq-Im0Xz26dhuZNBCv5hIaHy3iZAyUQTnToYMuU-nWLHeao9yyyDCJvBfjiAxU1qFko39w67M4ULGrwuOO9xHp6EBZqfYYhDjGmaOZNaCnrPf89TKIW2CvE9DG8Zby6J1EcnUhjUavBou8pFLv_3x0enIfjRD89STTunK83qHEEDqqz0nxAPh=w945-h600-p-k-no-nu" alt="Strawberries" />
+        <img class="pc-screenshot" src="https://blogger.googleusercontent.com/img/a/AVvXsEim0sV6_GUxPRfY1JpkU-XKmZRVLZ5PZlpaAXSLYOacAHGP3icTyFYIyDIq84s-5tYPHZNY_e39CeH1rsM3dkPWxSpWsn34mnpZMT3acx6FS38MsUOMXJbiSbpoCh_b9OnEwrEg7Yl6btv6FgitADo0zD-XEYWmcQ4dJ8RvksS9RQgnOaqUS5X05PoP0v-e=w640-h360" alt="Drawing Overload Screenshot" />
+    </div>
+    `,"#383838",!0),new e("project-4","Roomaker","https://img.itch.zone/aW1nLzEzNTUxNjYzLnBuZw==/315x250%23c/AqpKHb.png",`
+    <div class="paragraph">
+    <strong>Grand Theft Laugh</strong> Global Game Jam 2024.
+    Room Makers is a chill 2D puzzle game with a few friends for the GBJam 11.
+    In this gameboy themed game, you're a lucky guy who constantly receives new furniture but struggle to quickly arrange it all!
+    </div>
+    <div class="paragraph center">
+        <img src="https://franrosatti9.github.io/img/projects/RoomMakersGif.gif">
+    </div>
+       
+    <div class="paragraph">
+        Main features :
+        <ul>
+        <li>Theme: Space.</li>
+        <li>Time: 15 days.</li>
+        <li>Language: C#.</li>
+        <li>Engine: Unity 2022.</li>
+        <li>Platform: WebGL.</li>
+        <li>Other: Git.</li>
+        </ul>
+    </div>
+
+    <div class="paragraph">
+        <div class="notice">
+        Windows build available on <a href="https://justneki.itch.io/room-makers" target="_blank">itch.io</a>.
+        Android version coming soon!
+        </div>
+    </div>
+
+    <div class="paragraph center">
+        <img class="pc-screenshot" src="https://img.itch.zone/aW1hZ2UvMjI3NTEwNy8xMzQ5OTIzOC5wbmc=/347x500/guAo4v.png" alt="Eugeneable Screenshot" />
+        <img class="pc-screenshot" src="https://img.itch.zone/aW1hZ2UvMjI3NTEwNy8xMzUxMjY0MC5wbmc=/347x500/07%2F80q.png" alt="Eugeneable Screenshot" />
+        <img class="pc-screenshot" src="https://img.itch.zone/aW1hZ2UvMjI3NTEwNy8xMzUxMjc3Ni5wbmc=/347x500/PvCBd8.png" alt="Eugeneable Screenshot" />
+    </div>
+    `,"#e80fb7"),new e("project-6","Move & Move","https://img.itch.zone/aW1nLzE0NTk4NTIxLnBuZw==/315x250%23c/Y9OzJk.png",`
+        <div class="paragraph">
+            <strong>Move & Move Remake</strong> Remake of my first project following SOLID.
+        </div>
+         <div class="paragraph center">
+            <img src="img/projects/movemove.gif"
+        </div>
+        <div class="paragraph">
+            Simple 2D game of an spaceship and some aliens.
+            I remade my first game using what I've learnt in almost 7 years.
+            I Applied <strong>SOLID</strong> principles and Pattern Designs such as Singleton, Observer, Strategy and Pooling.
+        </div>
+        <div class="paragraph">
+            Main features :
+            <ul>
+                <li>Status: Done.</li>
+                <li>Language: C#.</li>
+                <li>Engine: Unity.</li>
+                <li>Platform: WebGL / Android.</li>
+            </ul>
+        </div>
+    
+        <div class="paragraph">
+            <div class="notice">
+            Windows build available on <a href="https://some.where/nice" target="_blank">itch.io</a>.
+            Source code available on <a href="https://github.com/yourself" target="_blank">GitHub</a>.
+            </div>
+        </div>
+    
+        <div class="paragraph center">
+            <img class="pc-screenshot" src="https://fakeimg.pl/534x300/" alt="Cloud Drew Land Screenshot" />
+            <img class="pc-screenshot" src="https://fakeimg.pl/534x300/" alt="Cloud Drew Land Screenshot" />
+            <img class="pc-screenshot" src="https://fakeimg.pl/534x300/" alt="Cloud Drew Land Screenshot" />
+        </div>`,"#e48246",!1,!1)],m=r({name:"GameProjects",components:{ProjectsList:i},data:function(){return{projects:h}}}),d=a("h1",null,"Game Projects",-1),u=a("div",{style:{"margin-bottom":"30px"}}," Most notable projects. ",-1),v=a("div",{style:{"margin-top":"20px"}},[g(" There is more to see on "),a("a",{target:"_blank",href:"https://github.com/Ulysses-Alv?tab=repositories"},"Github Repo")],-1);function f(s,b,w,y,j,k){const t=n("ProjectsList");return p(),l("div",null,[d,u,c(t,{projects:s.projects},null,8,["projects"]),v])}const _=o(m,[["render",f]]);export{_ as default};
