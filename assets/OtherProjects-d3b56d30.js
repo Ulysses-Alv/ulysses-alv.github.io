98d2ddb0c00360c896c2623106a07442857bdc9e
Ulises Alvarenga <ulises.j.alvarenga@gmail.com>
2024-08-13 12:56:45 -0300

    Updates

--- assets/OtherProjects-d3b56d30.js
+++ assets/OtherProjects-d3b56d30.js
@@ -1,4 +1,4 @@
-import{P as e,a as o}from"./ProjectData-53e5dd31.js";import{e as r,_ as i,h as l,c,i as n,a as t,o as p}from"./index-35c51598.js";const h=[new e("project-6","Sultans Of Sing","img/projects/project-6-icon.png",`
+import{P as e,a as o}from"./ProjectData-153f5f3f.js";import{e as r,_ as i,h as l,c,i as n,a as t,o as p}from"./index-c2cf42d0.js";const h=[new e("project-6","Sultans Of Sing","img/projects/project-6-icon.png",`
     <div class="paragraph">
       <strong>Sultans Of Sing</strong> is a thing of beauty that I am so proud of. It's available on Windows Store because why not.
       <br/>Image by <a href="https://www.pexels.com/fr-fr/@aldoalz" target="blank">Aldo Picaso</a>.
